fb7bf0cb1ba94a81dbbbeaa29aecb6792a65c604
gogaz <gogaz@gogaz.org>
2018-08-28 23:04:41 +0200

    Cleaning up

--- react_api/urls.py
+++ react_api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from front.views.clan import clans_list, clan_infos, clan_members
+from react_api.views.clan import clans_list, clan_infos, clan_members
 
 urlpatterns = [
     path('clan/', include([
