f4f85d94db673ed56ee5c1603a2e02421afe2c42
gogaz <gogaz@gogaz.org>
2018-09-10 12:10:34 +0200

    migrated cycom_esport2 to coach_royale

--- coach_royale/urls.py
+++ coach_royale/urls.py
@@ -1,4 +1,4 @@
-"""cycom_esport2 URL Configuration
+"""coach_royale URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
