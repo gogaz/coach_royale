90cf63ca27e2882d0998be88fb29b2097b5391b9
gogaz <gogaz@gogaz.org>
2018-09-11 11:31:26 +0200

    Set up requires.io

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-CircleCI [![CircleCI](https://circleci.com/gh/gogaz/coach_royale/tree/master.svg?style=svg)](https://circleci.com/gh/gogaz/coach_royale/tree/master)
+
+![CircleCI branch](https://img.shields.io/circleci/project/github/gogaz/coach_royale/master.svg)
 [![codecov](https://codecov.io/gh/gogaz/coach_royale/branch/master/graph/badge.svg)](https://codecov.io/gh/gogaz/coach_royale)
+[![Requirements Status](https://requires.io/github/gogaz/coach_royale/requirements.svg?branch=master)](https://requires.io/github/gogaz/coach_royale/requirements/?branch=master)
--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Switch, Link, Route, Redirect } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import ClanApp from "./clan/ClanApp";
 import TopBar from "./ui/TopBar";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 
 import "../style/app.css"
 import { handleErrors } from "../helpers/api";
@@ -14,38 +14,38 @@ library.add(fas);
 
 export default class App extends React.Component {
     constructor(props) {
-        super(props);
+      super(props);
 
-        this.state = {
+      this.state = {
             user: null,
-            defaultUrl: ''
-        }
+            defaultUrl: '',
     }
+  }
 
     componentDidMount() {
-        fetch('/api/home')
-            .then((res) => handleErrors(res))
-            .then((res) => this.setState({defaultUrl: res.url}))
-            .catch(error => console.log(error) );
-    }
+      fetch('/api/home')
+      .then(res => handleErrors(res))
+      .then(res => this.setState({ defaultUrl: res.url }))
+      .catch(error => console.log(error));
+  }
 
     render() {
-        return (
-            <div>
-                <TopBar user={this.state.user} />
-                <div className="app-body">
-                    <main className="main">
-                        <div className="container-fluid mt-3">
-                            <Switch>
-                                {this.state.defaultUrl && <Route exact path="/" component={() => <Redirect to={this.state.defaultUrl} />}/>}
-                                <Route path='/clan' component={ ClanApp } />
-                                <Route path='/player' component={ PlayerApp } />
-                            </Switch>
-                        </div>
-                    </main>
-                </div>
-                <footer>&nbsp;</footer>
+      return (
+      <div>
+          <TopBar user={this.state.user} />
+          <div className='app-body'>
+          <main className='main'>
+              <div className='container-fluid mt-3'>
+              <Switch>
+                  {this.state.defaultUrl && <Route exact path='/' component={() => <Redirect to={this.state.defaultUrl} />} />}
+                  <Route path='/clan' component={ClanApp} />
+                  <Route path='/player' component={PlayerApp} />
+                </Switch>
             </div>
-        )
-    }
+            </main>
+        </div>
+          <footer>&nbsp;</footer>
+        </div>
+    )
+  }
 }
