04d878219e792911a2d8945e2cc5dddfe93db88a
gogaz <gogaz@gogaz.org>
2018-10-04 19:43:10 +0200

    Added endpoint to refrersh open tournaments

--- react_api/views/tournaments.py
+++ react_api/views/tournaments.py
@@ -17,3 +17,24 @@ def playable_tournaments(request):
         refresh = TournamentRefresh.objects.filter(success=True).order_by('-timestamp').first()
         j = TournamentRefreshSerializer(refresh, tournaments=tournaments)
         return Response(j.data)
+
+
+@api_view(['POST'])
+def refresh_playable_tournaments(request):
+    def raise_error(msg, status=403):
+        return Response({'error': {'message': 'Your request cannot be proceeded, ' + msg}}, status=status)
+
+    if request.method == 'POST':
+        refresh = TournamentRefresh.objects.order_by('-timestamp').first()
+        refresh_s = TournamentRefresh.objects.filter(success=True).order_by('-timestamp').first()
+        if (refresh == refresh_s) and refresh.timestamp > timezone.now() - timezone.timedelta(minutes=15):
+            return raise_error("please try again later.")
+        elif refresh != refresh_s and not refresh.success and refresh.timestamp > timezone.now() - timezone.timedelta(seconds=15):
+            return raise_error("another request is already pending.")
+        else:
+            api_client = clashroyale.RoyaleAPI(settings.ROYALE_API_KEY, timeout=10)
+            result = refresh_open_tournaments(api_client)
+            if not result.success:
+                return raise_error(result.error, 500)
+            tournaments = TournamentSerializer(TournamentRepository.get_playable_tournaments(), many=True).data
+            return Response(TournamentRefreshSerializer(result, tournaments=tournaments).data)
