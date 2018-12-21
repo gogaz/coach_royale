ae48c335872ddb7f92d0fa3de6092d104f7acefa
gogaz <gogaz@gogaz.org>
2018-12-21 02:07:24 +0100

    Added models to admin site for debugging purpose

--- react_api/admin.py
+++ react_api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from react_api.models import *
+
+admin.site.register(Clan)
+admin.site.register(ClanHistory)
+admin.site.register(ClanWar)
+
+admin.site.register(Player)
+admin.site.register(PlayerClanStatsHistory)
+admin.site.register(PlayerStatsHistory)
+admin.site.register(PlayerClanWar)
+
+admin.site.register(Card)
+admin.site.register(Arena)
+admin.site.register(BattleMode)
+
+admin.site.register(Tournament)
+admin.site.register(TournamentRefresh)
