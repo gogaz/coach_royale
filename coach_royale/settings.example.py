34c9daae46b27fcba9ebaf01906eaf19a54a9fee
gogaz <gogaz@gogaz.org>
2019-04-04 12:31:06 +0200

    Fixed templates for admin panel in default settings file

--- coach_royale/settings.example.py
+++ coach_royale/settings.example.py
@@ -57,6 +57,24 @@ REST_FRAMEWORK = {
 
 ROOT_URLCONF = 'coach_royale.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            # template tags goes here
+            'libraries': {},
+        },
+    },
+]
+
 WSGI_APPLICATION = 'coach_royale.wsgi.application'
 
 
