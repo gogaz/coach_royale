c6ebf69db5d64b4ba9ec568ea91dbaf69d922b48
gogaz <gogaz@gogaz.org>
2020-08-21 21:39:21 +0200

    Fix context processor

--- backend/context_processor.py
+++ backend/context_processor.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 def global_context(request):
     return {
-        **settings.__dict__,
+        **settings.__dict__['_wrapped'].__dict__,
         'REFRESH_RATE': settings.REFRESH_RATE.total_seconds(),
     }
